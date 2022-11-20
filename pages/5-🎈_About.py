36f015109649243e51d445eaf2ea4a9ed4822f1e
DaG0ng <dagong@usc.edu>
2022-11-20 06:58:23 -0800

    Update 5-🎈_About.py

---
+++ pages/5-🎈_About.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(layout='wide')
+
+st.title('About [Database Code](https://github.com/DaG0ng/Block_Chain_Database)')
+
+st.header("[Documentations](https://github.com/DaG0ng/Block_Chain_Database/blob/main/papers/DSC534%20Project%20Progress%20Proposal.pdf)")
+
+st.header("References:")
+st.write("“What Is Blockchain?” Gartner, https://www.gartner.com/en/articles/what-is-blockchain")
+st.write("“The future of blockchain” IBM, https://www.ibm.com/blogs/blockchain/2020/04/the-future-of-blockchain/")
